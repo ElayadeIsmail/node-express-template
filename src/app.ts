4df5e0487b38d1a164f69b3ee5bb45b68cdabe05
Elayade Ismail <elayadeismail@gmail.com>
2024-09-03 22:44:07 +0100

    add config

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import loggerMiddleware from './middlewares/logger.middleware';
 
 const app = express();
 
 app.use(express.json());
+app.use(loggerMiddleware.successHandler);
+app.use(loggerMiddleware.errorHandler);
 
 app.get('/', (_, res) => {
   res.send('Hello world');
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
 import { app } from './app';
-import { envVars } from './config';
+import { envVars, logger } from './config';
 
-const server = app.listen(envVars.PORT, () => {
-  console.log(`Server running at http://localhost:${envVars.PORT}`);
+const server = app.listen(envVars.port, () => {
+  logger.info(`Server running at http://localhost:${envVars.port}`);
 });
 
-// listen for SIGTERM signal ^C is
-// used to gracefully shutdown a Node.js server
 process.on('SIGTERM', () => {
-  console.log('Received SIGTERM. Shutting down..');
+  logger.info('Received SIGTERM. Shutting down..');
   server.close();
 });
