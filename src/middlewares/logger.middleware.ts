d04e07472d810e1839a0dc21018e5deb6356b484
Elayade Ismail <elayadeismail@gmail.com>
2024-09-03 22:43:54 +0100

    add logger middleware

---
+++ src/middlewares/logger.middleware.ts
@@ -0,0 +1,30 @@
+import { Response } from 'express';
+import fs from 'fs';
+import morgan from 'morgan';
+import path from 'path';
+import { envVars } from '../config';
+
+morgan.token('message', (_, res: Response) => res.locals.errorMessage || '');
+
+const getIPFormat = () =>
+  envVars.env === 'production' ? ':remote-addr - ' : '';
+
+const accessLogStream = fs.createWriteStream(
+  path.join(process.cwd(), 'logs/access.log'),
+  { flags: 'a' }
+);
+
+const successResponseFormat = `${getIPFormat()} :method :url :status :response-time ms :user-agent :date`;
+
+const successHandler = morgan(successResponseFormat, {
+  stream: envVars.env === 'production' ? accessLogStream : process.stdout,
+  skip: (_, res) => res.statusCode >= 400,
+});
+
+const errorResponseFormat = `${getIPFormat()} :method :url :status :response-time ms :user-agent :date - error-message: :message`;
+const errorHandler = morgan(errorResponseFormat, {
+  stream: envVars.env === 'production' ? accessLogStream : process.stdout,
+  skip: (_, res) => res.statusCode < 400,
+});
+
+export default { errorHandler, successHandler };
