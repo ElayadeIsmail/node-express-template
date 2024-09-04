c841bd65f70ac976eb88705d5ad93d10f7500de7
Elayade Ismail <elayadeismail@gmail.com>
2024-09-04 11:55:29 +0100

    add db client

--- src/database/client.ts
+++ src/database/client.ts
@@ -1,8 +1,9 @@
+import { envVars } from '@/config';
+import * as schema from '@/schemas';
 import Database from 'better-sqlite3';
 import { drizzle } from 'drizzle-orm/better-sqlite3';
-import { envVars } from '../config';
 
 const sqlite = new Database(envVars.dbUrl);
-const db = drizzle(sqlite);
+const db = drizzle(sqlite, { schema });
 
 export { db };
