f6f68d0c008e94c8d967ae67ea4d81c2b36a1304
Elayade Ismail <elayadeismail@gmail.com>
2024-09-04 19:55:27 +0100

    add validation

--- src/lib/validation/auth.validator.ts
+++ src/lib/validation/auth.validator.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 const usernameSchema = z
-  .string()
+  .string({ required_error: 'Username is required' })
   .min(4, { message: 'Username must be at least 4 characters long' })
   .max(12, { message: 'Username must be at max 12 characters long' })
   .regex(/^[a-zA-Z_]+$/, {
@@ -9,11 +9,14 @@ const usernameSchema = z
   });
 
 const passwordSchema = z
-  .string()
-  .regex(/^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/, {
-    message:
-      'Password must be at least 8 characters long, and include at least one uppercase letter, one number, and one special character',
-  });
+  .string({ required_error: 'Password is required' })
+  .regex(
+    /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*.,;:])[A-Za-z\d!@#$%^&*.,;:]{8,}$/,
+    {
+      message:
+        'Password must be at least 8 characters long, and include at least one uppercase letter, one number, and one special character',
+    }
+  );
 
 export const loginSchema = z.object({
   username: usernameSchema,
