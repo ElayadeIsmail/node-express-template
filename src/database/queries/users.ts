a36b59b6eb066bc7f7acbc734160ac935d6e1b9f
Elayade Ismail <elayadeismail@gmail.com>
2024-09-06 19:52:56 +0100

    add users query

--- src/database/queries/users.ts
+++ src/database/queries/users.ts
@@ -1,8 +1,8 @@
 import { users } from '@/database/schemas';
-import { eq } from 'drizzle-orm';
+import { eq, or } from 'drizzle-orm';
 import { db } from '../client';
 
-const findByUsername = async (username: string) => {
+const findByUsernameOrEmail = async (usernameOrEmail: string) => {
   const [user] = await db
     .select({
       id: users.id,
@@ -13,9 +13,55 @@ const findByUsername = async (username: string) => {
       createdAt: users.createdAt,
     })
     .from(users)
-    .where(eq(users.username, username))
+    .where(
+      or(eq(users.username, usernameOrEmail), eq(users.email, usernameOrEmail))
+    )
     .limit(1);
   return user;
 };
 
-export default { findByUsername };
+const findById = async (id: string) => {
+  const [user] = await db
+    .select({
+      id: users.id,
+      username: users.username,
+      email: users.email,
+      password: users.password,
+    })
+    .from(users)
+    .where(eq(users.id, id))
+    .limit(1);
+  return user;
+};
+
+interface User {
+  username: string;
+  email: string;
+  password: string;
+}
+
+const create = async (user: User) => {
+  const [newUser] = await db.insert(users).values(user).returning({
+    id: users.id,
+    username: users.username,
+  });
+  return newUser;
+};
+
+const update = async (id: string, user: Partial<User>) => {
+  const [updatedUser] = await db
+    .update(users)
+    .set(user)
+    .where(eq(users.id, id))
+    .returning({
+      id: users.id,
+      username: users.username,
+    });
+  return updatedUser;
+};
+
+const remove = async (id: string) => {
+  await db.delete(users).where(eq(users.id, id));
+};
+
+export default { findByUsernameOrEmail, findById, create, update, remove };
